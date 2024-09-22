d4fc25b86a1e1b8af9be9f17e2c6bbc2a5d61d9c
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2024-09-22 13:43:54 +0100

    Add cloudinary config

---
+++ cloudinary.config.ts
@@ -0,0 +1,16 @@
+import cloudinary from 'cloudinary';
+
+// Manually importing environment variables with default values
+const CLOUDINARY_API_NAME =
+  process.env.CLOUDINARY_API_NAME || 'default_cloud_name';
+const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || 'default_api_key';
+const CLOUDINARY_API_SECRET =
+  process.env.CLOUDINARY_API_SECRET || 'default_api_secret';
+
+cloudinary.v2.config({
+  cloud_name: CLOUDINARY_API_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
+
+export const cloudinaryInstance = cloudinary.v2.uploader;
